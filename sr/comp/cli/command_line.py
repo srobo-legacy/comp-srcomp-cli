fa1b68d311fa4f66f28a141c176ca8c19c20ede6
Alistair Lynn <arplynn@gmail.com>
2015-02-14 16:33:00 +0000

    Add the knocked out teams script

--- sr/comp/cli/command_line.py
+++ sr/comp/cli/command_line.py
@@ -3,12 +3,14 @@ import sys
 import argparse
 
 from . import print_schedule
+from . import knocked_out_teams
 
 def argument_parser():
     """A parser for CLI tool command line arguments, from argparse."""
     parser = argparse.ArgumentParser(description='srcomp command-line interface')
     subparsers = parser.add_subparsers(title='commands')
     print_schedule.add_subparser(subparsers)
+    knocked_out_teams.add_subparser(subparsers)
     return parser
 
 
---
+++ sr/comp/cli/knocked_out_teams.py
@@ -0,0 +1,25 @@
+from __future__ import print_function
+
+from sr.comp.comp import SRComp
+import os.path
+
+def command(settings):
+    comp = SRComp(os.path.realpath(settings.compstate))
+
+    teams_last_round = set()
+    for i, matches in enumerate(comp.schedule.knockout_rounds):
+        teams_this_round = set()
+        for game in matches:
+            teams_this_round.update(game.teams)
+
+        print("Teams not in round {}".format(i))
+        out = teams_last_round - teams_this_round
+        print(", ".join(t for t in out if t is not None))
+        teams_last_round = teams_this_round
+        print()
+
+def add_subparser(subparsers):
+    parser = subparsers.add_parser('knocked-out-teams',
+                                   help='show the teams knocked out of each knockout round')
+    parser.add_argument('compstate', help='competition state repository')
+    parser.set_defaults(func=command)
