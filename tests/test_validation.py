ace3ff2b07a53b2d50236d6bdf18a1a25958f711
Peter Law <PeterJCLaw@gmail.com>
2015-02-28 21:42:59 +0000

    Add experimental testing of the validation command
    This is deliberatly testing it as a command-line thing, and isn't
    really aimed at testing whether it picks up on issues in the state.
    That testing is done in the srcomp layer.
    
    Change-Id: I09a18c328723194897789bc735811e551719b6a6

---
+++ tests/test_validation.py
@@ -0,0 +1,11 @@
+
+import subprocess
+
+def test_dummy_is_valid():
+    test_dir = os.path.dirname(os.path.abspath(__file__))
+    dummy_compstate = os.path.join(test_dir, 'dummy')
+    try:
+        subprocess.check_output(['srcomp', 'validate', dummy_compstate], \
+                                stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as cpe:
+        assert cpe.returncode == 0, cpe.output
