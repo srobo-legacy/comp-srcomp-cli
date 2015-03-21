48f7395ac5e422cb60c9333165cb98562845bd8d
Peter Law <PeterJCLaw@gmail.com>
2015-03-21 18:55:39 +0000

    Tidyup command_line code
    Alphabetically sort imports and parser addition, wrap overly long
    help message.
    
    Change-Id: I11c0e976df0ecae458d3c6291f95017294e2dfbd

--- sr/comp/cli/command_line.py
+++ sr/comp/cli/command_line.py
@@ -2,17 +2,17 @@
 
 from __future__ import print_function
 
-import sys
 import argparse
+import sys
 
-from . import print_schedule
-from . import knocked_out_teams
-from . import validate
-from . import schedule_league
-from . import import_teams
-from . import awards
 from . import add_delay
+from . import awards
+from . import import_teams
+from . import knocked_out_teams
+from . import print_schedule
+from . import schedule_league
 from . import scorer
+from . import validate
 from . import yaml_round_trip
 
 def add_list_commands(subparsers):
@@ -20,8 +20,10 @@ def add_list_commands(subparsers):
         commands = subparsers.choices.keys()
         print(" ".join(commands))
 
-    parser = subparsers.add_parser('list-commands',
-                                   help='Lists the available commands; useful for adding auto-completion of command names')
+    help_text = 'Lists the available commands; useful for adding ' \
+                'auto-completion of command names'
+
+    parser = subparsers.add_parser('list-commands', help=help_text)
     parser.set_defaults(func=command)
 
 def argument_parser():
@@ -29,15 +31,17 @@ def argument_parser():
     parser = argparse.ArgumentParser(description='srcomp command-line interface')
     subparsers = parser.add_subparsers(title='commands')
     add_list_commands(subparsers)
-    print_schedule.add_subparser(subparsers)
-    knocked_out_teams.add_subparser(subparsers)
-    validate.add_subparser(subparsers)
-    schedule_league.add_subparser(subparsers)
-    import_teams.add_subparser(subparsers)
-    awards.add_subparser(subparsers)
+
     add_delay.add_subparser(subparsers)
+    awards.add_subparser(subparsers)
+    import_teams.add_subparser(subparsers)
+    knocked_out_teams.add_subparser(subparsers)
+    print_schedule.add_subparser(subparsers)
+    schedule_league.add_subparser(subparsers)
     scorer.add_subparser(subparsers)
+    validate.add_subparser(subparsers)
     yaml_round_trip.add_subparser(subparsers)
+
     return parser
 
 
