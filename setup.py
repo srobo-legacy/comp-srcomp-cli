67310f214d5b189ded81bc54945a4d8d94c12d2c
Tom Leese <inbox@tomleese.me.uk>
2015-04-17 07:22:16 +0100

    Add 'pathlib' backport for Python 2 support

--- setup.py
+++ setup.py
@@ -1,8 +1,25 @@
+import sys
+
 from setuptools import find_packages, setup
 
 
 with open('README.rst') as f:
     long_description = f.read()
+
+install_requires = [
+    'paramiko >=1.10, <2',
+    'sr.comp >=1.0, <2',
+    'reportlab >=3.1.44, <4',
+    'requests >=2.5.1, <3',
+    'ruamel.yaml >=0.5.1, <0.6',
+    'simplejson >=3.6, <4',
+    'six >=1.9, <2',
+    'timelib >=0.2.4, <0.3',
+    'Pillow >=2.7, <3'
+]
+
+if sys.version_info < (3, 4):
+    install_requires.append('pathlib >=1.0, <2')
 
 setup(
     name='sr.comp.cli',
@@ -13,17 +30,7 @@ setup(
     long_description=long_description,
     author='Student Robotics Competition Software SIG',
     author_email='srobo-devel@googlegroups.com',
-    install_requires=[
-        'paramiko >=1.10, <2',
-        'sr.comp >=1.0, <2',
-        'reportlab >=3.1.44, <4',
-        'requests >=2.5.1, <3',
-        'ruamel.yaml >=0.5.1, <0.6',
-        'simplejson >=3.6, <4',
-        'six >=1.9, <2',
-        'timelib >=0.2.4, <0.3',
-        'Pillow >=2.7, <3'
-    ],
+    install_requires=install_requires,
     entry_points={
         'console_scripts': [
             'srcomp = sr.comp.cli.command_line:main'
